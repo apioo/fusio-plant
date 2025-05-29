0c09e808361e295129abcabb2531dd481e5f96f9
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-05-29 21:24:48 +0200

    start all

--- install.sh
+++ install.sh
@@ -124,7 +124,7 @@ if [ $? -ne 0 ]; then
     echo "> certbot --nginx --agree-tos -m \"info@$domain\" -d \"$domain\""
     echo ""
 fi
-supervisord
+supervisorctl start all
 echo ""
 echo "Fusio Plant successfully installed"
 echo ""
