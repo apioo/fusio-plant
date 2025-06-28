6b147bc50f7a94c06e00cf2b9dfcca336c267b74
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-06-28 14:53:40 +0200

    update link

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This file is part of the Fusio Plant project (https://fusio-project.org/product/plant).
+# This file is part of the Fusio Plant project (https://github.com/apioo/fusio-plant).
 # Fusio Plant is a server control panel to easily self-host apps on your server.
 #
 # Copyright (c) Christoph Kappestein <christoph.kappestein@gmail.com>
